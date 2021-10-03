beb53574a862e7a075b0b0f89bd776bd43f3c331
iiaii <jhun9409@naver.com>
2021-10-03 23:00:52 +0900

    [step2] feat, refactor: NumberUtils에서 String -> List<Integer> 변환기능 추가

--- src/main/java/baseball/util/NumberUtils.java
+++ src/main/java/baseball/util/NumberUtils.java
@@ -2,18 +2,28 @@ package baseball.util;
 
 import baseball.exception.NotNumberException;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class NumberUtils {
 
     private static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+(\\.\\d+)?");
 
-    public static int requireNumber(final String number) {
-        return Optional.ofNullable(number)
-                .filter(NumberUtils::isNumber)
-                .map(Integer::parseInt)
-                .orElseThrow(() -> new NotNumberException(number));
+    public static List<Integer> convertToNumbers(final String numberString) {
+        requireNumber(numberString);
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < numberString.length(); i++) {
+            String number = numberString.substring(i, i + 1);
+            numbers.add(Integer.parseInt(number));
+        }
+        return numbers;
+    }
+
+    private static void requireNumber(final String number) {
+        if (!isNumber(number)) {
+            throw new NotNumberException(number);
+        }
     }
 
     private static boolean isNumber(final String number) {
