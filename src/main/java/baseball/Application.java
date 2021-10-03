44deffa1d241870b3f79b412cd2ce6824c39149e
iiaii <jhun9409@naver.com>
2021-10-03 23:03:44 +0900

    [step2] feat: 숫자야구 메인 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,51 @@
 package baseball;
 
+import baseball.domain.BallResult;
+import baseball.domain.Balls;
+import baseball.domain.Computer;
+import baseball.domain.GameMenu;
+import baseball.exception.*;
+import baseball.view.BallNumbersConsoleInput;
+import baseball.view.BallResultConsoleOutput;
+import baseball.view.ErrorMessageConsoleOutput;
+import baseball.view.GameMenuConsoleInput;
+
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        do {
+            playGame();
+        } while (isNewGame());
     }
+
+    private static boolean isNewGame() {
+        try {
+            String menu = GameMenuConsoleInput.askMenu(GameMenu.indexes(), GameMenu.menus());
+            return GameMenu.isNewGame(menu);
+        } catch(BaseBallException e) {
+            ErrorMessageConsoleOutput.print(e.getMessage());
+            return isNewGame();
+        }
+    }
+
+    private static void playGame() {
+        Balls targetBalls = new Balls(Computer.createNumbers());
+        BallResult ballResult;
+        do {
+            ballResult = targetBalls.compare(newBalls());
+            BallResultConsoleOutput.print(ballResult.strikeCount(), ballResult.ballCount());
+        } while (!ballResult.isAllStrike());
+        BallResultConsoleOutput.printGameEnd(Balls.SIZE);
+    }
+
+    private static Balls newBalls() {
+        try {
+            return new Balls(BallNumbersConsoleInput.askNumbers());
+        } catch(BaseBallException e) {
+            ErrorMessageConsoleOutput.print(e.getMessage());
+            return newBalls();
+        }
+    }
+
 }
